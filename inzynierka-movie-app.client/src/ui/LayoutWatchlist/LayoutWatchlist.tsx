ad239ecd1ab7ec0295e219db90591fce969c2712
AcePeQ <maciej.nojszewski22@gmail.com>
2024-09-07 13:56:47 +0200

    feat(fix): fix TypeList folder in structure moved to ui folder

--- inzynierka-movie-app.client/src/ui/LayoutWatchlist/LayoutWatchlist.tsx
+++ inzynierka-movie-app.client/src/ui/LayoutWatchlist/LayoutWatchlist.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import styles from "./LayoutWatchlist.module.css";
-import TypeList from "../../features/Watchlist/TypeList/TypeList";
+import TypeList from "../TypeList/TypeList";
 import CategoryList from "../../features/Watchlist/CategoryList/CategoryList";
 
 function LayoutWatchlist() {
