dc066814a231c55bc0f255b770db25b0c1662038
AcePeQ <maciej.nojszewski22@gmail.com>
2024-09-11 16:17:23 +0200

    feat: MovieHero component with styles

--- inzynierka-movie-app.client/src/features/Movie/MovieHero/MovieHero.tsx
+++ inzynierka-movie-app.client/src/features/Movie/MovieHero/MovieHero.tsx
@@ -1,5 +1,6 @@
 import { HiMiniStar } from "react-icons/hi2";
 import styles from "./MovieHero.module.css";
+import Button from "../../../ui/Button/Button";
 
 function MovieHero() {
   const backgroundStyles = {
@@ -18,11 +19,6 @@ function MovieHero() {
         <div className={styles.informationsContainer}>
           <div className={styles.informationsHeader}>
             <p className={styles.title}>Shogun</p>
-            <div className={styles.ratingBox}>
-              <HiMiniStar />
-              <span className={styles.rating}>9,5</span>
-            </div>
-            <span className={styles.totalRates}>(154k)</span>
           </div>
           <div className={styles.additionalInformationsContainer}>
             <p className={styles.additionalInformations}>
@@ -31,10 +27,18 @@ function MovieHero() {
           </div>
           <div className={styles.detailsContainer}>
             <div className={styles.detailRow}>
+              <p className={styles.detailTitle}>Rating</p>
+              <div className={styles.ratingBox}>
+                <HiMiniStar />
+                <span className={styles.rating}>9,5</span>
+              </div>
+            </div>
+            <div className={styles.detailRow}>
               <p className={styles.detailTitle}>Your rating</p>
               <div className={styles.ratingBox}>
                 <HiMiniStar />
                 <span className={styles.rating}>9,5</span>
+                <span className={styles.totalRates}>(154k)</span>
               </div>
             </div>
             <div className={styles.detailRow}>
@@ -43,7 +47,7 @@ function MovieHero() {
             </div>
             <div className={styles.detailRow}>
               <p className={styles.detailTitle}>Age rating</p>
-              <p className={styles.detailValue}>59 min</p>
+              <p className={styles.detailValue}>+18</p>
             </div>
             <div className={styles.detailRow}>
               <p className={styles.detailTitle}>Country</p>
@@ -53,6 +57,20 @@ function MovieHero() {
               <p className={styles.detailTitle}>Genres</p>
               <p className={styles.detailValue}>Action, Historical, War</p>
             </div>
+          </div>
+
+          <p className={styles.description}>
+            „Shogun” to opowieść, która ukazuje nastroje panujące w Japonii w
+            1600 roku. To wówczas kraj bez centralnej władzy, brodzący na
+            przełomie okresów Sengoku (walczących prowincji) i Edo, w którym
+            obejmujący władzę ród miał wyznaczyć kierunek kraju na następne
+            dwieście lat.
+          </p>
+
+          <div className={styles.buttons}>
+            <Button size="medium" type="primary">
+              Add to list
+            </Button>
           </div>
         </div>
       </div>
