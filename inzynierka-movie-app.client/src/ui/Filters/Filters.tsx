1726205d62c3a42f8fa3f77095ad655040b6b967
AcePeQ <maciej.nojszewski22@gmail.com>
2024-09-12 18:05:16 +0200

    feat: filter sliders with styles

--- inzynierka-movie-app.client/src/ui/Filters/Filters.tsx
+++ inzynierka-movie-app.client/src/ui/Filters/Filters.tsx
@@ -1,22 +1,44 @@
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
+import ReactSlider from "react-slider";
 import Filter from "./Filter/Filter";
 import styles from "./Filters.module.css";
-import { useState } from "react";
 
 function Filters() {
-  const [yearFilter, setYearFiler] = useState([1890, 2024]);
-
   return (
     <div className={styles.filters}>
       <Filter title="Year">
         <div className={`${styles.filterContainer} ${styles.filterYear}`}>
-          <Slider range min={1890} max={2024} value={[...yearFilter]} />
+          <ReactSlider
+            className="horizontal-slider-years"
+            thumbClassName="slider-thumb"
+            trackClassName="slider-track"
+            defaultValue={[1890, 2024]}
+            min={1890}
+            max={2024}
+            renderThumb={(props, state) => (
+              <div {...props}>{state.valueNow}</div>
+            )}
+            pearling
+            minDistance={0}
+          />
         </div>
       </Filter>
       <Filter title="Rating">
         <div className={`${styles.filterContainer} ${styles.filterRating}`}>
-          Elo
+          <ReactSlider
+            className="horizontal-slider-rating"
+            thumbClassName="slider-thumb"
+            trackClassName="slider-track"
+            markClassName="slider-mark"
+            defaultValue={[0, 10]}
+            marks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+            min={1}
+            max={10}
+            renderThumb={(props, state) => (
+              <div {...props}>{state.valueNow}</div>
+            )}
+            pearling
+            minDistance={0}
+          />
         </div>
       </Filter>
       <Filter title="Genres">
