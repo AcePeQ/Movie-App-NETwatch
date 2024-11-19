dd709c1e423074646c86a4a6f05d21bc44521937
AcePeQ <maciej.nojszewski22@gmail.com>
2024-11-19 16:33:34 +0100

    feat(fix): profile menu working

--- inzynierka-movie-app.client/src/ui/ProfileMenu/ProfileMenu.tsx
+++ inzynierka-movie-app.client/src/ui/ProfileMenu/ProfileMenu.tsx
@@ -2,7 +2,7 @@ import Avatar from "../Avatar/Avatar";
 
 import styles from "./ProfileMenu.module.css";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { HiUserCircle } from "react-icons/hi2";
 import { HiOutlineViewList } from "react-icons/hi";
@@ -21,9 +21,18 @@ function ProfileMenu() {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  function handleClickTemp() {
-    setIsOpen((open) => !open);
-  }
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClose = (e: MouseEvent) => {
+      if (!menuRef?.current?.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClose);
+    return () => document.removeEventListener("mousedown", handleClose);
+  }, []);
 
   function handleLogout() {
     navigate("/");
@@ -31,8 +40,19 @@ function ProfileMenu() {
   }
 
   return (
-    <div className={styles.profileMenu}>
-      <div className={styles.profile} onClick={handleClickTemp}>
+    <div
+      className={styles.profileMenu}
+      onClick={(e) => {
+        e.stopPropagation();
+      }}
+      ref={menuRef}
+    >
+      <div
+        className={styles.profile}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
         <Avatar type="profile" />
         <p className={styles.profileName}>{user?.username}</p>
         {isOpen ? (
@@ -42,45 +62,49 @@ function ProfileMenu() {
         )}
       </div>
 
-      {isOpen && (
-        <div className={styles.dropdown}>
-          <ul className={styles.itemList}>
-            <Link
-              className={styles.dropdownItem}
-              to={`/user/${user?.username}`}
-            >
-              <li className={styles.item}>
-                <HiUserCircle />
-                <p className={styles.title}>Profile</p>
-              </li>
-            </Link>
+      <div
+        className={`${styles.dropdown} ${
+          styles[isOpen ? "active" : "inActive"]
+        }`}
+      >
+        <ul
+          className={styles.itemList}
+          onClick={() => {
+            setIsOpen(false);
+          }}
+        >
+          <Link className={styles.dropdownItem} to={`/user/${user?.username}`}>
+            <li className={styles.item}>
+              <HiUserCircle />
+              <p className={styles.title}>Profile</p>
+            </li>
+          </Link>
 
-            <Link className={styles.dropdownItem} to="/">
-              <li className={styles.item}>
-                <HiOutlineViewList />
-                <p className={styles.title}>Watchlist</p>
-              </li>
-            </Link>
+          <Link className={styles.dropdownItem} to="/">
+            <li className={styles.item}>
+              <HiOutlineViewList />
+              <p className={styles.title}>Watchlist</p>
+            </li>
+          </Link>
 
-            <Link
-              className={styles.dropdownItem}
-              to={`/user/${user?.username}/settings`}
-            >
-              <li className={styles.item}>
-                <RiSettings5Fill />
-                <p className={styles.title}>Settings</p>
-              </li>
-            </Link>
+          <Link
+            className={styles.dropdownItem}
+            to={`/user/${user?.username}/settings`}
+          >
+            <li className={styles.item}>
+              <RiSettings5Fill />
+              <p className={styles.title}>Settings</p>
+            </li>
+          </Link>
 
-            <div className={styles.dropdownItem} onClick={handleLogout}>
-              <li className={styles.item}>
-                <HiOutlineLogout />
-                <p className={styles.title}>Logout</p>
-              </li>
-            </div>
-          </ul>
-        </div>
-      )}
+          <div className={styles.dropdownItem} onClick={handleLogout}>
+            <li className={styles.item}>
+              <HiOutlineLogout />
+              <p className={styles.title}>Logout</p>
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
   );
 }
